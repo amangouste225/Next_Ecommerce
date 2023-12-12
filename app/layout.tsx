fd19fa6a3cdb52f84f161075d7a4e663fd0e09e5
Amangoua Ange <adagou@yahoo.com>
2023-12-12 18:10:34 +0000

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
